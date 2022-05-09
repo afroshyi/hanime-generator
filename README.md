241830aeb1f65730e9b2fc04147e6610d1d96468
afroshyi <afrozakram11@gmail.com>
2022-05-09 16:24:00 -0400

    hanime gen
    this repo will now open your default browser with a random hentai link from a custom picked selection

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
  this will grab hanime token (fr)
 
 this will not actually grab shit, this is so rdimo can give me access to his git hub account
+
+run this file and it finna gib u random hentai (ill add more links later)
--- grabber.py
+++ grabber.py
@@ -1,34 +1,10 @@
-import discord
+import webbrowser
 import random
-import os
-import asyncio
-import os, random
-from discord.ext import commands
-from discord.ext.commands import cooldown, BucketType
-from pkg_resources import WorkingSet
 
+with open("hentai.txt","r") as f:
+    urls = f.readlines()
 
-client = commands.Bot(command_prefix=["send "])
+new=2
 
-@client.event
-async def on_ready():
-    print('hentai sender is ready') 
-
-
-@client.command()
-async def hentai(ctx):
-        await randomSpam("uncanon", ctx)
-
-
-async def randomSpam(name, ctx):
-    stored = []
-    for subdirs, dirs, files in os.walk(name):
-        for file in files:
-            filepath = subdirs + os.sep + file
-            stored.append(os.path.abspath(filepath))
-    list = random.sample(stored, 10) # change this number to change the number of hentai sent
-    for i in list:
-        await ctx.send(file=discord.File(i))
-        await asyncio.sleep(0.8) # change the 0.8 value to change delay
-
-client.run('token here')   
+url= random.choice(urls)
+webbrowser.open(url,new= new);
